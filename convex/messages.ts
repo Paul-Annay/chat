f4b11d338eef54b7fbd9931be776354cf07f934e
Annay Paul <annay.paul@g.bracu.ac.bd>
2024-02-15 02:05:09 +0600

    Chat App

---
+++ convex/messages.ts
@@ -0,0 +1,49 @@
+import { query, mutation } from "./_generated/server";
+import { v } from "convex/values";
+
+export const list = query({
+    args: {},
+    handler: async (ctx) => {
+        // Grab the most recent messages.
+        const messages = await ctx.db.query("messages").order("desc").take(100);
+        const messagesWithLikes = await Promise.all(
+            messages.map(async (message) => {
+                const likes = await ctx.db
+                    .query("likes")
+                    .withIndex("byMessageId", (q) =>
+                        q.eq("messageId", message._id)
+                    )
+                    .collect();
+
+                return {
+                    ...message,
+                    likes: likes.length,
+                };
+            })
+        );
+        // Reverse the list so that it's in a chronological order.
+        return messagesWithLikes.reverse().map((msg) => ({
+            ...msg,
+            body: msg.body.replace(":)", "😄"),
+        }));
+    },
+});
+
+export const send = mutation({
+    args: { body: v.string(), author: v.string() },
+    handler: async (ctx, { body, author }) => {
+        // Send a new message.
+        await ctx.db.insert("messages", { body, author });
+    },
+});
+
+export const like = mutation({
+    args: { liker: v.string(), messageId: v.id("messages") },
+    handler: async (ctx, args) => {
+        // Like a message
+        await ctx.db.insert("likes", {
+            liker: args.liker,
+            messageId: args.messageId,
+        });
+    },
+});
---
+++ convex/schema.ts
@@ -0,0 +1,13 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+    messages: defineTable({
+        author: v.string(),
+        body: v.string(),
+    }),
+    likes: defineTable({
+        messageId: v.id("messages"),
+        liker: v.string(),
+    }).index("byMessageId", ["messageId"]),
+});
